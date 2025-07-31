0d06f5946b0556f60b507eaf1431264752206f4b
mazi-kunle <102045752+mazi-kunle@users.noreply.github.com>
2025-07-31 14:18:19 +0100

    Add files via upload

--- data_reader.py
+++ data_reader.py
@@ -16,50 +16,50 @@ def file_reader(file_path: str):
 	'''
 	data = {}
 
-	# Get all sheet names
-	sheet_names = pd.ExcelFile(file_path).sheet_names
+	with pd.ExcelFile(file_path) as xls:
+		# Get all sheet names
+		sheet_names = xls.sheet_names
 
-	for sheet in sheet_names:
-		df = pd.read_excel(file_path, sheet_name=sheet)
+		for sheet in sheet_names:
+			df = pd.read_excel(file_path, sheet_name=sheet)
 
 		
-		columns = df.columns.tolist()
-		print(columns)
+			columns = df.columns.tolist()
+			print(columns)
 
-		if len(columns) != 4:
-			raise ValueError(f'Sheet:"{sheet}" in {file_path} is not properly formatted, Kindly read the documentation.')
-			return 0
+			if len(columns) != 4:
+				raise ValueError(f'Sheet:"{sheet}" in {file_path} is not properly formatted, Kindly read the documentation.')
 
-		try:
-			# get temperature as key
-			key = str(int(df[columns[0]].tolist()[0]))
-			
-			# get thickness
-			thickness =df[columns[1]].tolist()[0]
-			
+			try:
+				# get temperature as key
+				key = str(int(df[columns[0]].tolist()[0]))
+				
+				# get thickness
+				thickness =df[columns[1]].tolist()[0]
+				
 
-			# get time
-			time = np.array(df[columns[2]].tolist())
-			
-			# get moisture ratio
-			MR = np.array(df[columns[3]].tolist())
+				# get time
+				time = np.array(df[columns[2]].tolist())
+				
+				# get moisture ratio
+				MR = np.array(df[columns[3]].tolist())
 
-		except Exception as e:
-			raise ValueError(f'Sheet:"{sheet}" in {file_path} is not properly formatted, Kindly read the documentation.')
+			except Exception as e:
+				raise ValueError(f'Sheet:"{sheet}" in {file_path} is not properly formatted, Kindly read the documentation.')
 
 
-		else:
-			# extract data into data dictionary
-			new_data = {
-				'time': time,
-				'MR' : MR
-			}
+			else:
+				# extract data into data dictionary
+				new_data = {
+					'time': time,
+					'MR' : MR
+				}
 
-		if key in data.keys():
-			data[key][thickness] = new_data
-		else:
-			data[key] = {
-				thickness: new_data
-			}
+			if key in data.keys():
+				data[key][thickness] = new_data
+			else:
+				data[key] = {
+					thickness: new_data
+				}
 
 	return data
